5b53d7e706539492591972748f52a8fb49429a47
MC-yexuan <86520882+MC-yexuan@users.noreply.github.com>
2022-04-07 16:20:28 +0800

    Rename suanfa.cpp to 正则表达式匹配问题 (AL).cpp

--- 正则表达式匹配问题 (AL).cpp
+++ 正则表达式匹配问题 (AL).cpp
@@ -3,30 +3,30 @@
 #include<string.h>
 using namespace std;
     
-    int k1,k2=-1;int flag[127];//¼ÇÂ¼ASCII ******
+    int k1,k2=-1;int flag[127];//è®°å½•ASCII ******
     class word
     {
     public:
-        int num,flag1,flag2;//flag1´Ó×óµ½ÓÒ£¬flag2´ÓÓÒµ½×ó¡£
+        int num,flag1,flag2;//flag1ä»å·¦åˆ°å³ï¼Œflag2ä»å³åˆ°å·¦ã€‚
     };
-    int mid[8];//¡®-¡¯»ØËİ
-    int n;//ĞĞÊı
+    int mid[8];//â€˜-â€™å›æº¯
+    int n;//è¡Œæ•°
     char c1;
-    //fg=0,Ê×Î²£»fg=1£¬£¿£»fg=2£¬*£»
+    //fg=0,é¦–å°¾ï¼›fg=1ï¼Œï¼Ÿï¼›fg=2ï¼Œ*ï¼›
     int main(){
-    ifstream in("input.txt");//ÒÔ»Ø³µ½áÎ²
+    ifstream in("input.txt");//ä»¥å›è½¦ç»“å°¾
     ofstream out("output.txt");
     memset(flag,-1,sizeof(flag));
     word cword[127];
     int eof=in.eof();
     while(!eof)
     {
-        k1++;//¼ÆÊı
-        in.get(c1);             //È¡³öÎÄ¼şÖĞµÄ×ÖÄ¸
-        eof=in.eof();           //Ö¸ÏòÏÂÒ»¸ö
+        k1++;//è®¡æ•°
+        in.get(c1);             //å–å‡ºæ–‡ä»¶ä¸­çš„å­—æ¯
+        eof=in.eof();           //æŒ‡å‘ä¸‹ä¸€ä¸ª
         if(c1==' '||c1=='+') continue;
         else if(c1=='\n')
-        {k1=-1; k2++; memset(flag,-1,sizeof(flag));}    //ÖØĞÂ¼ÆÊı
+        {k1=-1; k2++; memset(flag,-1,sizeof(flag));}    //é‡æ–°è®¡æ•°
         else if(c1=='-'){       
             for(int i=k1-1;i>=0;i--)
                 cword[mid[i]].num=-100;
@@ -34,7 +34,7 @@ using namespace std;
         }
         else {
             mid[k1]=(int)c1;//ASCII
-            flag[mid[k1]]++;//ÏàÍ¬ÔªËØ¼ÇÒ»´Î
+            flag[mid[k1]]++;//ç›¸åŒå…ƒç´ è®°ä¸€æ¬¡
             if(cword[mid[k1]].flag1<k1)
             cword[mid[k1]].flag1=k1;//flag1
             if(!flag[mid[k1]])
